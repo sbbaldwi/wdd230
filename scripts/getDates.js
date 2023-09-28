f88dbb52e48a0e549b0682464c44a0ef31715808
sbbaldwin <bal20020@byui.edu>
2023-09-28 11:25:33 -0700

    lot

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,4 @@ var lastModificationDate = new Date(document.lastModified);
 time.innerHTML= new Date().getFullYear();
 let lastModified = document.getElementById("lastModificationDate");
 lastModified.innerHTML= lastModificationDate;
+
