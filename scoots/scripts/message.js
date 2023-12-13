b21962bebf4e3df9e97b974170e602d62f21648b
sbbaldwin <bal20020@byui.edu>
2023-12-13 07:39:32 -0800

    stuffs

--- scoots/scripts/message.js
+++ scoots/scripts/message.js
@@ -1,45 +1,24 @@
-const apiKey = 'c6788a8407a7cc25e447409c0992dd7a';
-const city = 'Cozumel';
-const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = 'c6788a8407a7cc25e447409c0992dd7a';
+    const city = 'Cozumel';
 
-function displayTemperatureMessage() {
-    fetch(weatherAPI)
+    var closeButton = document.querySelector('#temperature-message .close');
+    var temperatureMessage = document.getElementById('temperature-message');
+    var highTemperature = document.getElementById('high-temperature');
+
+    // Fetch temperature data
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             const tempMax = data.main.temp_max;
-            const messageContainer = document.getElementById('temperature-message');
-            const highTemperatureElement = document.getElementById('high-temperature');
-
-            if (tempMax) {
-                highTemperatureElement.textContent = tempMax + '°C';
-                messageContainer.style.display = 'block';
-            }
+            highTemperature.textContent = `${tempMax} °C`;
         })
-        .catch(error => console.error('Error fetching temperature:', error));
-}
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            highTemperature.textContent = 'N/A';
+        });
 
-function closeTemperatureMessage() {
-    const messageContainer = document.getElementById('temperature-message');
-    messageContainer.style.display = 'none';
-}
-
-function displayTemperatureMessage() {
-    fetch(weatherAPI)
-        .then(response => response.json())
-        .then(data => {
-            const tempMax = data.main.temp_max;
-            const messageContainer = document.getElementById('temperature-message');
-            const highTemperatureElement = document.getElementById('high-temperature');
-
-            if (tempMax !== undefined && tempMax !== null) {
-                highTemperatureElement.textContent = tempMax + '°C';
-                messageContainer.style.display = 'block';
-            } else {
-                messageContainer.style.display = 'none'; // Hide the message if no temperature data
-            }
-        })
-        .catch(error => console.error('Error fetching temperature:', error));
-}
-
-
-window.onload = displayTemperatureMessage;
+    closeButton.addEventListener('click', function () {
+        temperatureMessage.style.display = 'none';
+    });
+});
