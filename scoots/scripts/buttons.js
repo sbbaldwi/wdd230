544bd0c68b475dec14bc0daec7cfb3aa7ab4eede
sbbaldwin <bal20020@byui.edu>
2023-12-13 14:35:25 -0800

    buttons

---
+++ scoots/scripts/buttons.js
@@ -0,0 +1,16 @@
+document.getElementById("homepageButton").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+
+document.getElementById("rentalseButton").addEventListener("click", function () {
+    window.location.href = "reservations.html";
+});
+
+document.getElementById("section2").addEventListener("click", function () {
+    window.location.href = "rentals.html";
+});
+
+document.getElementById("section2").addEventListener("click", function () {
+    window.location.href = "reservations.html";
+});
+
