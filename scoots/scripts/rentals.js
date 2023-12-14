657f287db76ff19c58692652f66cbee5965b84ca
sbbaldwin <bal20020@byui.edu>
2023-12-13 19:18:31 -0800

    json derulo

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -1,108 +1,43 @@
-const url = 'data/rentals.JSON';
-const cards = document.querySelector('#cards');
+function createRentalItem(item) {
+    var rentalDiv = document.createElement('div');
+    rentalDiv.classList.add('rental-card'); // Assign the 'rental-card' class
 
-async function getRentalData() {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Network response was not ok.');
-        }
-        const data = await response.json();
-        displayRental(data.rentals);
-    } catch (error) {
-        console.error('Error fetching or parsing JSON:', error);
-    }
+    var img = document.createElement('img');
+    img.src = item.imageURL;
+    rentalDiv.appendChild(img);
+
+    var name = document.createElement('h2'); // Use h2 for the rental name
+    name.textContent = item.name;
+    rentalDiv.appendChild(name);
+
+    var category = document.createElement('p');
+    category.textContent = "Category: " + item.category;
+    rentalDiv.appendChild(category);
+
+    var maxPersons = document.createElement('p');
+    maxPersons.textContent = "Max Persons: " + item.max_persons;
+    rentalDiv.appendChild(maxPersons);
+
+    var halfDayRes = document.createElement('p');
+    halfDayRes.textContent = "Half Day Reservation: $" + item.half_day_reservation;
+    rentalDiv.appendChild(halfDayRes);
+
+    var fullDayRes = document.createElement('p');
+    fullDayRes.textContent = "Full Day Reservation: $" + item.full_day_reservation;
+    rentalDiv.appendChild(fullDayRes);
+
+    document.getElementById('rentalsContainer').appendChild(rentalDiv);
 }
 
-getRentalData();
-
-const displayRental = (rentals) => {
-    rentals.forEach((rental) => {
-        const rentalCard = document.createElement('div');
-        rentalCard.classList.add('rental-card');
-
-        const image = document.createElement('img');
-        image.setAttribute('src', rental.imageURL);
-        image.setAttribute('alt', `Vehicle Type ${rental.name}`);
-        image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '340');
-        image.setAttribute('height', '440');
-        rentalCard.appendChild(image);
-
-        const rentalName = document.createElement('h2');
-        rentalName.textContent = rental.name;
-        rentalCard.appendChild(rentalName);
-
-        const rentalCategory = document.createElement('p');
-        rentalCategory.textContent = `Category: ${rental.category}`;
-        rentalCard.appendChild(rentalCategory);
-
-        const rentalMaxPersons = document.createElement('p');
-        rentalMaxPersons.textContent = `Max Persons: ${rental.max_persons}`;
-        rentalCard.appendChild(rentalMaxPersons);
-
-        const rentalReservation = document.createElement('p');
-        rentalReservation.textContent = `Reservation (Half/Full Day): $${rental.half_day_reservation}/$${rental.full_day_reservation}`;
-        rentalCard.appendChild(rentalReservation);
-
-        const rentalWalkIn = document.createElement('p');
-        rentalWalkIn.textContent = `Walk-In (Half/Full Day): $${rental.half_day_walk_in}/$${rental.full_day_walk_in}`;
-        rentalCard.appendChild(rentalWalkIn);
-
-        cards.appendChild(rentalCard);
+// Fetch rental data
+fetch('https://raw.githubusercontent.com/sbbaldwi/wdd230/main/scoots/data/rentals.JSON')
+    .then(response => response.json())
+    .then(data => {
+        // Loop through the rentals and display each item
+        data.rentals.forEach(item => {
+            createRentalItem(item);
+        });
+    })
+    .catch(error => {
+        console.log('Error fetching data:', error);
     });
-}
-
-
-
-/*document.addEventListener("DOMContentLoaded", function () {
-    fetch('/data/rentals.json')
-        .then(response => response.json())
-        .then(data => displayRentals(data.rentals))
-});
-
-function displayRentals(rentals) {
-    const rentalsContainer = document.getElementById('rentalsContainer');
-
-    rentals.forEach(rental => {
-        const rentalCard = document.createElement('div');
-        rentalCard.classList.add('rental-card');
-
-        const rentalImage = document.createElement('img');
-        rentalImage.src = rental.imageURL;
-        rentalImage.alt = rental.name;
-        rentalCard.appendChild(rentalImage);
-
-        const rentalName = document.createElement('h2');
-        rentalName.textContent = rental.name;
-        rentalCard.appendChild(rentalName);
-
-        const rentalCategory = document.createElement('p');
-        rentalCategory.textContent = `Category: ${rental.category}`;
-        rentalCard.appendChild(rentalCategory);
-
-        const rentalMaxPersons = document.createElement('p');
-        rentalMaxPersons.textContent = `Max Persons: ${rental.max_persons}`;
-        rentalCard.appendChild(rentalMaxPersons);
-
-        const rentalReservation = document.createElement('p');
-        rentalReservation.textContent = `Reservation (Half/Full Day): $${rental.half_day_reservation}/$${rental.full_day_reservation}`;
-        rentalCard.appendChild(rentalReservation);
-
-        const rentalWalkIn = document.createElement('p');
-        rentalWalkIn.textContent = `Walk-In (Half/Full Day): $${rental.half_day_walk_in}/$${rental.full_day_walk_in}`;
-        rentalCard.appendChild(rentalWalkIn);
-
-        rentalsContainer.appendChild(rentalCard);
-    });
-}
-
-
-
-function Func() {
-    fetch("data/rental.json")
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => console.log(data));
-}*/
