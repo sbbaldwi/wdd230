e45273e17c90d299b6c42d8d4573a39f061eded8
sbbaldwin <bal20020@byui.edu>
2023-12-13 16:46:47 -0800

    fixes

---
+++ scoots/scripts/hammenuhome.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu-home");
+const heroHeader = document.querySelector(".hero-container");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    heroHeader.classList.toggle("menu-open");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    heroHeader.classList.remove("menu-open");
+}))
